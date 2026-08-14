610b4feeb6302ea4b2b9bb7669e7b12d25c49f3b
Divya <divyaarmarkar@gmail.com>
2026-08-14 14:27:04 +0900

    Complete JDBC assignment implementation

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,34 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // implement algorithm here
+        UserService userService = new UserServiceImpl();
+
+        //1.create user table
+        userService.createUsersTable();
+
+        //2.Add users to database( console outputs will print automatically via Saveuser)
+        userService.saveUser("Jia","Khan",(byte)20);
+        userService.saveUser("Alice","Smith",(byte) 22);
+        userService.saveUser("Bob","Oclean",(byte) 30);
+        userService.saveUser("Henry","Brown",(byte) 45);
+
+        //3.Get all users from database and print them to comsole
+        List<User> users = userService.getAllUsers();
+        for (User user : users){
+            System.out.println(user);
+        }
+
+        //4.Clean the Users table
+        userService.cleanUsersTable();
+
+        //5.Drop the Users table
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
 public class Util {
-    // set up a database connection
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/jdbc_test",
+                "root",
+                "Travelikediva@154"
+        );
+    }
 }
+
+
